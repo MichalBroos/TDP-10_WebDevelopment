9b8f83f47e12e0cb8d0560c24d1893589af9b7ee
Michal Broos <2330994b@gmail.com>
2022-05-24 16:52:52 +0100

    Add intro JavaScript files

---
+++ js_intro.js
@@ -0,0 +1,16 @@
+let x = 6;
+console.log(x);
+
+let obj = {
+    name: "Name",
+    number: 12345
+}
+console.log(obj);
+
+console.log("con" + "cat");
+
+function add(a, b) {
+    return a + b;
+}
+console.log(add("adding", "strings"));
+console.log("adding numbers:", add(1, 2));
