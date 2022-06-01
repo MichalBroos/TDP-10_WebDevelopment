9d2b232d94154dd095824a0194be548daf15ed83
Michal Broos <2330994b@gmail.com>
2022-06-01 12:42:22 +0100

    Add HTML form for adding a new kitten C in Kittens CRUD

--- kittens.js
+++ kittens.js
@@ -1,1 +1,11 @@
 "use strict"
+axios.get("http://localhost:4494/kitten/getAll")
+    .then(response => {
+        //
+    })
+    .catch(error => console.error(err));
+
+/* Notes:
+age < 0 ? 0 : age
+reset using loop and wipe all input children of form - no need button type reset in html does this for you
+*/
