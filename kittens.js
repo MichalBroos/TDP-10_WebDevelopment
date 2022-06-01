4cce6e914222f189c4c0c2f9ffdb8e039368856e
Michal Broos <2330994b@gmail.com>
2022-06-01 16:40:59 +0100

    Complete CR in Kittens CRUD

--- kittens.js
+++ kittens.js
@@ -1,9 +1,4 @@
 "use strict"
-// axios.get("http://localhost:4494/kitten/getAll")
-//     .then(response => {
-//         //
-//     })
-//     .catch(error => console.error(err));
 
 document.getElementById("kittenForm").addEventListener("submit", function (event) {
     // to avoid using default form method = GET = appending data to action URL
@@ -18,7 +13,33 @@ document.getElementById("kittenForm").addEventListener("submit", function (event
         cuteness: this.kittenCuteness.value < 0 ? 0 : this.kittenCuteness.value
     }
     console.log(newKitten);
+
+    axios.post("http://localhost:4494/kitten/create", newKitten)
+        .then(response => {
+            console.log(response);
+            viewAllKittens();
+        })
+        .catch(error => console.error(error));
 });
+
+let viewAllKittens = () => {
+    axios.get("http://localhost:4494/kitten/getAll")
+        .then(response => {
+            const kittenView = document.getElementById("kittenView");
+            // restore legend for fieldset
+            kittenView.innerHTML = kittenView.getElementsByTagName("legend")[0].outerHTML;
+            for (let kitten of response.data) {
+                console.log(kitten.id);
+                let kittenP = document.createElement("p");
+                kittenP.innerText = `Name: ${kitten.name}\nAge:${kitten.age}
+                                     Breed:${kitten.breed}\nCuteness:${kitten.cuteness}`;
+                kittenView.prepend(kittenP);
+            }
+        })
+        .catch(error => console.error(error));
+}
+// show all kittens on load
+viewAllKittens();
 
 /* Notes:
 age < 0 ? 0 : age
