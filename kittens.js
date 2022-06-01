0b39d410466e701f2dde93c91ca8f8b714415797
Michal Broos <2330994b@gmail.com>
2022-06-01 13:05:32 +0100

    Add newKitten object to kittenForm event listener

--- kittens.js
+++ kittens.js
@@ -1,9 +1,24 @@
 "use strict"
-axios.get("http://localhost:4494/kitten/getAll")
-    .then(response => {
-        //
-    })
-    .catch(error => console.error(err));
+// axios.get("http://localhost:4494/kitten/getAll")
+//     .then(response => {
+//         //
+//     })
+//     .catch(error => console.error(err));
+
+document.getElementById("kittenForm").addEventListener("submit", function (event) {
+    // to avoid using default form method = GET = appending data to action URL
+    event.preventDefault();
+
+    // in this case, this means element that triggered the event = kittenForm
+    console.log(this);
+    const newKitten = {
+        name: this.kittenName.value,
+        age: this.kittenAge.value < 0 ? 0 : this.kittenAge.value,
+        breed: this.kittenBreed.value,
+        cuteness: this.kittenCuteness.value < 0 ? 0 : this.kittenCuteness.value
+    }
+    console.log(newKitten);
+});
 
 /* Notes:
 age < 0 ? 0 : age
